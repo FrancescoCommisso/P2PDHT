231ac328185d258a6ccc64a64ba73e7c9ccd3dd0
Francesco Commisso <francescocommissodev@gmail.com>
2019-04-07 20:24:07 -0400

    broken

--- src/Main.java
+++ src/Main.java
@@ -6,39 +6,43 @@ public class Main {
 
         ServerPool sp = new ServerPool();
 
-        Client client1 = new Client(1, Constants.CLIENT_IP_1);
-        Client client2 = new Client(2, Constants.CLIENT_IP_2);
-        Client client3 = new Client(3, Constants.CLIENT_IP_3);
-        Client client4 = new Client(4, Constants.CLIENT_IP_4);
+        Client client1 = new Client(11, Constants.CLIENT_IP_1);
+        Client client2 = new Client(12, Constants.CLIENT_IP_2);
+        Client client3 = new Client(13, Constants.CLIENT_IP_3);
+        Client client4 = new Client(14, Constants.CLIENT_IP_4);
 
         client1.init();
         client2.init();
         client3.init();
         client4.init();
 
-        client2.informAndUpdate("picture of a dag");
-        client1.informAndUpdate("a");
-        client2.informAndUpdate("b");
-        client2.informAndUpdate("selfie123");
-        client1.informAndUpdate("grad");
-        client3.informAndUpdate("c");
-        client4.informAndUpdate("d");
-        client4.informAndUpdate("a red car");
-        client2.informAndUpdate("the monalisa");
-        client3.informAndUpdate("a tiger");
+        client1.informAndUpdate("/Users/Francesco/Desktop/P2PImages/a.jpeg");
+        client2.informAndUpdate("/Users/Francesco/Desktop/P2PImages/b.jpeg");
+        client3.informAndUpdate("/Users/Francesco/Desktop/P2PImages/c.jpeg");
+        client4.informAndUpdate("/Users/Francesco/Desktop/P2PImages/d.jpeg");
 
+
+        client1.queryForContent("c.jpeg");
+
+        System.out.println("\nAFTER PLACING ALL IMAGES THE DIRECTORY SERVERS LOOK LIKE:\n");
 
         for (DirectoryServer ds : sp.getDirectoryServers()) {
             ds.printAllRecords();
         }
 
-        client1.queryForContent("a red car asd ");
+        client1.fileTransfer("c.jpeg");
+        System.out.println("\nAFTER FILE TRANSFERS, ALL IMAGES THE DIRECTORY SERVERS LOOK LIKE:\n");
+        for (DirectoryServer ds : sp.getDirectoryServers()) {
+            ds.printAllRecords();
+        }
+
 
         client1.exit();
         client2.exit();
         client3.exit();
         client4.exit();
 
+        System.out.println("\nAFTER EXITING, ALL IMAGES THE DIRECTORY SERVERS LOOK LIKE:\n");
 
         for (DirectoryServer ds : sp.getDirectoryServers()) {
             ds.printAllRecords();
